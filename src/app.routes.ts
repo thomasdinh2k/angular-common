03423484f716de17d220deda82deabfe8bb98e45
thomasdinh <work.dnstung@gmail.com>
2026-03-12 21:26:27 +0700

    chore: setup route

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -4,13 +4,14 @@ import { Dashboard } from './app/pages/dashboard/dashboard';
 import { Documentation } from './app/pages/documentation/documentation';
 import { Landing } from './app/pages/landing/landing';
 import { Notfound } from './app/pages/notfound/notfound';
+import { Empty } from '@/app/pages/empty/empty';
 
 export const appRoutes: Routes = [
     {
         path: '',
         component: AppLayout,
         children: [
-            { path: '', component: Dashboard },
+            { path: '', component: Empty },
             { path: 'uikit', loadChildren: () => import('./app/pages/uikit/uikit.routes') },
             { path: 'documentation', component: Documentation },
             { path: 'pages', loadChildren: () => import('./app/pages/pages.routes') }
